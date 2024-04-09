5020930dd62de6b2adaf98bfc721842834ba17fc
Stephen C. <38684083+NeroThroN@users.noreply.github.com>
2024-04-10 00:08:16 +0200

    Add URL strategy for web routing

--- lib/router/config.router.dart
+++ lib/router/config.router.dart
@@ -1,11 +1,15 @@
 import 'package:go_router/go_router.dart';
 import 'package:meeseeks/router/routes.router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 part 'config.router.g.dart';
 
 @riverpod
 Raw<GoRouter> router(RouterRef ref) {
+  // Remove the leading # in the web url
+  setPathUrlStrategy();
+
   // Enable the imperative navigation API based on the URL
   // Migration after go_router v8.0.0
   // https://docs.google.com/document/d/1VCuB85D5kYxPR3qYOjVmw8boAGKb7k62heFyfFHTOvw/edit
