44f74b8f25a2ee9ae8bacc68a4144f14b95ca838
Stephen C. <38684083+NeroThroN@users.noreply.github.com>
2024-04-10 13:58:43 +0200

    Add Loading screen & logic

--- lib/router/routes.router.dart
+++ lib/router/routes.router.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meeseeks/screens/home.screen.dart';
+import 'package:meeseeks/screens/loading.screen.dart';
 
 part 'routes.router.g.dart';
 
-@TypedGoRoute<HomeRoute>(path: '/')
+/// The key of the router root
+final rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+
+@TypedShellRoute<RootShell>(
+  routes: [
+    TypedGoRoute<HomeRoute>(path: '/'),
+  ],
+)
+class RootShell extends ShellRouteData {
+  const RootShell();
+
+  static final GlobalKey<NavigatorState> $navigatorKey = rootNavigatorKey;
+
+  @override
+  Widget builder(context, state, navigator) => LoadingScreen(key: state.pageKey, child: navigator);
+}
+
 class HomeRoute extends GoRouteData {
   @override
   Widget build(context, state) => HomeScreen(key: state.pageKey);
---
+++ lib/screens/loading.screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:meeseeks/style/themes.style.dart';
+
+/// A loading screen that displays a splash screen and a loading spinner
+class LoadingScreen extends StatefulWidget {
+  final Widget child;
+
+  const LoadingScreen({super.key, required this.child});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  /// Indicates if the splash animation ended
+  bool _splashEnded = false;
+
+  /// Indicates if all futures are done loading
+  bool _loadEnded = false;
+
+  /// Indicates if the screen should be loading
+  bool get loading => !_splashEnded || !_loadEnded;
+
+  @override
+  void initState() {
+    reset();
+    super.initState();
+  }
+
+  // Reset the loading screen
+  void reset() {
+    setState(() {
+      _splashEnded = false;
+      _loadEnded = false;
+    });
+
+    // Handles the end of the splash screen animation (minimum 2 seconds)
+    Future.delayed(const Duration(seconds: 2)).then((_) => setState(() => _splashEnded = true));
+
+    // Launch the loading
+    _load();
+  }
+
+  /// Load all futures
+  Future<void> _load() async {
+    // End of the loading
+    setState(() => _loadEnded = true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show child widget (Other screens)
+    if (!loading) return widget.child;
+
+    // Otherwise display the splash screen
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: CircularProgressIndicator(
+          strokeCap: StrokeCap.round,
+          color: Theme.of(context).colorScheme.onPrimary,
+          strokeWidth: 6,
+        ),
+      ),
+    );
+  }
+}
